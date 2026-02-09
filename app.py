c986c2939f75a68f50974d62cdf5a9b036d6f037
KeenishaKannan <keenisha.kannan@chitose-bio.com>
2026-02-09 16:24:06 +0900

    Update last updated caption in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 
 st.set_page_config(page_title="CAL Sales Trend Analysis Dashboard", layout="wide")
 st.title("CAL Sales Trend Analysis Dashboard")
-st.caption("This Dashboard was last updated on 4th February 17:30pm.")
+st.caption("Last updated on 9th February 16:30pm.")
 
 
 
