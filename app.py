b0f5e62d0c06533e11cf627983ddfc7485b5e06f
KeenishaKannan <keenisha.kannan@chitose-bio.com>
2026-03-11 15:18:06 +0900

    Update caption for dashboard status

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import re
 
 st.set_page_config(page_title="CAL Sales Trend Analysis Dashboard", layout="wide")
 st.title("CAL Sales Trend Analysis Dashboard")
-st.caption("Last updated on 11th March 15:20pm.")
-st.caption("Latest update: Added January 2026 data")
+st.caption("UNDER UPDATE!")
+
 
 EXCEL_FILE = "CAL Sales Data for Dashboard.xlsx"
 
